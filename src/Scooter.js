691f6e6c0c6c741fa66a2e1f4957bc34f9006275
Tiffany <139176913+talph101@users.noreply.github.com>
2024-09-13 15:15:00 -0500

    Scooter properties and tests created.

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,14 @@
 class Scooter {
   // scooter code here
+  static nextSerial = 1;
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial;
+    Scooter.nextSerial;
+    this.charge = 0;
+    this.isBroken = true;
+  }
 }
 
 module.exports = Scooter
--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -7,12 +7,38 @@ describe('scooter object', () => {
     expect(scooter).toBeInstanceOf(Scooter)
   })
 })
-
+//if you hard code something you don't pass it through
+//the constructor as a parameter
 // Method tests
-describe('scooter methods', () => {
+describe('scooter properties', () => {
   // tests here!
+  const thisScooter = new Scooter("station")
+  test("station is a property of Scooter", () => {
+    expect(thisScooter.station).toBe("station");
+  })
+
+  test("user who checked out scooter", () => {
+    expect(thisScooter.user).toEqual(null);
+  })
+
+  test("tests is serial is a property of Scooter", () => {
+    expect(thisScooter.serial).toEqual(Scooter.nextSerial);
+  })
+
+  test("nextSerial is static number", () => {
+    expect(Scooter.nextSerial).toEqual(1);
+  })
+
+  test("charge is a number", () => {
+    expect(typeof thisScooter.charge).toBe('number');
+    expect(thisScooter.charge).toBeGreaterThanOrEqual(0);
+    expect(thisScooter.charge).toBeLessThanOrEqual(100);
+  })
 
   // rent method
+  // test("rent method", () => {
+  //   expect()
+  // })
 
   // dock method
 
